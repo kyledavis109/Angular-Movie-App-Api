1d4e7bca88273c5a9252a3f05a7d011dbb3b3832
Kyle Davis <kyledavis109@gmail.com>
2022-10-05 17:06:41 -0500

    added route comment.

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 app.use(express.json())
 require('dotenv').config();
 
+// Route for getting trending movies of the week.
 app.get('/trending', async (req, res) => {
     try {
-        const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`;
+        const url = `https://api.themoviedb.org/3/trending/week/day?api_key=${process.env.API_KEY}`;
         const results = await makeReq(url, 'GET');
         return res.status(200).json(results.results[0].title);
     } catch(err) {

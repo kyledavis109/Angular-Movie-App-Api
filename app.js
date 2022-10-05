e50743bb8a43e4a21f17835e2513a31d08e2509e
Kyle Davis <kyledavis109@gmail.com>
2022-10-05 15:21:28 -0500

    Added api call route

--- app.js
+++ app.js
@@ -5,52 +5,15 @@ dotenv.config();
 app.use(express.json())
 require('dotenv').config();
 const fetch = require('node-fetch');
+const { makeReq } = require('./helperFunctions.js');
 
-async function getToken() {
-    try {
-        const url = `https://api.themoviedb.org/3/authentication/token/new?api_key=${process.env.API_KEY}`;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.status !== 200) {
-            throw await response.json();
-        };
-        const results = await response.json();
-        return results.request_token;
-        // console.log(results.request_token)
-        // console.log(results.expires_at)
-    } catch(err) {
-        console.log(err)
-        throw 'Failed to get request token.';
-    };
-}
-
-getToken()
-
-async function getTrending() {
+app.get('/trending', async (req, res) => {
     try {
         const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.status !== 200) {
-            throw await response.json();
-        };
-        const results = await response.json();
-        // return results
-        console.log(results.results[0].title);
+        const results = await makeReq(url, 'GET');
+        return res.status(200).json(results.results[0].title);
     } catch(err) {
         console.log(err)
-        throw 'Failed to get access token.';
+        return res.status(500).send('Server failed to fetch trending movies');
     };
-}
-
-getTrending()
+});

e249fa2f8ee63299669d3d514271edc8c8edbc96
Kyle Davis <kyledavis109@gmail.com>
2022-10-05 15:21:42 -0500

    added request function to api

---
+++ helperFunctions.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+// Function that makes API request.
+async function makeReq(url, method) {
+    const response = await fetch(url, {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+        }
+    });
+    
+    return await response.json();
+};
+
+    module.exports = {
+        makeReq,
+    };
+
+    async function getToken() {
+        try {
+            const url = `https://api.themoviedb.org/3/authentication/token/new?api_key=${process.env.API_KEY}`;
+            const results = await makeReq(url, 'GET');
+            return res.status(200).json(results.request_token);
+            // console.log(results.request_token)
+            // console.log(results.expires_at)
+        } catch(err) {
+            console.log(err)
+            return res.status(500).send('Server failed to request token');
+        };
+    };

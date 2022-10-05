9fc0da3f816e953da811b312fe54f2cf1c04d2f3
Kyle Davis <kyledavis109@gmail.com>
2022-10-05 14:25:43 -0500

    commit

--- app.js
+++ app.js
@@ -2,3 +2,31 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
+app.use(express.json())
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+async function getToken() {
+    try {
+        const url = `https://api.themoviedb.org/3/authentication/token/new?api_key=${process.env.API_KEY}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.status !== 200) {
+            throw await response.json();
+        };
+        const results = await response.json();
+        // return results.request_token;
+        console.log(results.request_token)
+        console.log(results.expires_at)
+    } catch(err) {
+        console.log(err)
+        throw 'Failed to get access token.';
+    };
+}
+
+getToken()

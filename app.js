a9ff8bc556497551b9663a55fcca4338dbe8a70f
Kyle Davis <kyledavis109@gmail.com>
2022-10-08 19:33:48 -0500

    added new routes for titles

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.use(function (req, res, next) {
 // Route for getting trending TV shows of the day's titles.
 app.get('/topTvTodayTitles', async (req, res) => {
     try {
+        const url = `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.API_KEY}`;
+        const results = await makeReq(url, 'GET');
+        return res.status(200).json(results.results);
+    } catch(err) {
+        console.log(err)
+        return res.status(500).send('Server failed to fetch trending movies');
+    };
+});
+
+// Route for getting trending movies of the day's titles.
+app.get('/topMoviesTodayTitles', async (req, res) => {
+    try {
         const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`;
         const results = await makeReq(url, 'GET');
         return res.status(200).json(results.results);
